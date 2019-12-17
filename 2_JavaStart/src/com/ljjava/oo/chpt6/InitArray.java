84a3f1ca602e45273211ae29f5584bd3467ff973
jin.liu <jackyliujin@qq.com>
2019-12-17 19:31:25 +0800

    add new files

---
+++ 2_JavaStart/src/com/ljjava/oo/chpt6/InitArray.java
@@ -0,0 +1,20 @@
+package com.ljjava.oo.chpt6;
+
+public class InitArray {
+    public static void main(String[] args){
+        // 声明一个数组
+        int[] a;
+
+        a= new int[5];
+
+        int[] b= new int[5]; // 声明的同时，指向一个数组
+
+        a[0]=1;
+        a[1]=2;
+        a[2]=3;
+        a[3]=4;
+        a[4]=5;
+
+        System.out.println(a.length);
+    }
+}
